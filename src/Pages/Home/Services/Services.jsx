50a9e2387c0ebac745949aca2eeff786d1bebc31
Md Yeasin <mdyeasinhn@gmail.com>
2024-06-21 20:32:32 +0600

    initial file created

---
+++ src/Pages/Home/Services/Services.jsx
@@ -0,0 +1,15 @@
+
+
+const Services = () => {
+    return (
+        <div className="mt-5">
+            <div className="text-center">
+                <h2 className="text-xl text-orange-400 font-bold">Service</h2>
+                <h className="text-5xl font-bold">Our Service Area</h>
+                <p>the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which don't look even slightly believable. </p>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
